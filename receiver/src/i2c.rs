b3ff0ec952479ad1f31aa98315da065c1e8e6d0a
Lexag <lexa.grebnets@gmail.com>
2026-07-24 12:21:15 +0200

    fix: rename spi to i2c everywhere

--- receiver/src/i2c.rs
+++ receiver/src/i2c.rs
@@ -1,23 +1,23 @@
 use crate::renderer::DisplayBuffer;
 use rppal::i2c;
 
-pub struct SpiDriver {
-    spi: i2c::I2c,
+pub struct I2CDriver {
+    i2c: i2c::I2c,
 }
 
-impl SpiDriver {
+impl I2CDriver {
     pub fn new() -> Self {
         let mut i = i2c::I2c::new().unwrap();
         i.set_slave_address(0x30);
-        Self { spi: i }
+        Self { i2c: i }
     }
 
     pub fn send_buffer(&mut self, img: DisplayBuffer) {
         let mut brights = [0; 32];
         brights[1..].clone_from_slice(&img.brightnesses);
         brights[0] = img.clock_divider;
-        self.spi.write(&brights);
-        self.spi.write(&img.reds.bits);
-        self.spi.write(&img.greens.bits);
+        self.i2c.write(&brights);
+        self.i2c.write(&img.reds.bits);
+        self.i2c.write(&img.greens.bits);
     }
 }
